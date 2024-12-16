9d3e696744a64c63f7c51b5eb908825997b8c7d7
Your Name <adityaraj642003@gmail.com>
2024-12-17 00:08:38 +0530

    lect03 updated

---
+++ lect-03.js
@@ -0,0 +1,11 @@
+let score = "33"
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score); 
+console.log(typeof valueInNumber)
+
+
+"33" //numebr
+"33abc" //naN (nt a number )
+boolean // true :1 false 0 
