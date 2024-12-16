74ebcbc1ffc6917e9e4bf12c4767a0a1259475e5
Your Name <adityaraj642003@gmail.com>
2024-12-16 23:31:24 +0530

    lect01notes

--- lect01.js
+++ lect01.js
@@ -1,1 +1,34 @@
 //hello world 
+// console.log("Hitesh")
+
+const accountId = 144553
+let accountEmail =  "aditya@google.com"
+var accountPasword = "12345"
+accountCity = "Noida"
+
+
+
+accountEmail =  "adi@priya.in"
+accountPasword = "2121211"
+accountCity = "Bengaluru"
+
+// console.log(accountId)
+// accountId = 2 is not posible since it is an constant 
+
+
+// console.log(accountEmail);
+// console.log(accountPasword);
+// console.log(accountCity);
+
+//console.table -> giv all the vars in tables
+console.table([accountId,accountEmail,accountCity, accountPasword])
+
+
+
+
+
+
+
+
+
+
---
+++ lect01.md
@@ -0,0 +1,9 @@
+# VAriables and constants
+
+const -> like kuch constants fixed hain mtb we cannot change now 
+
+
+<!-- var is not used now due to scope issues -->
+
+Prefer not to use var 
+because of isseu in block scope and fucntional scope
