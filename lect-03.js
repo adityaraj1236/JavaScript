a577225a2c4c409cc41b118bbd7d63c427a41469
Your Name <adityaraj642003@gmail.com>
2024-12-17 11:20:55 +0530

    operators

--- lect-03.js
+++ lect-03.js
@@ -1,11 +1,83 @@
 let score = "33"
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score); 
-console.log(typeof valueInNumber)
+// console.log(typeof valueInNumber)
 
 
-"33" //numebr
-"33abc" //naN (nt a number )
-boolean // true :1 false 0 
+// "33" //numebr
+// "33abc" //naN (nt a number )
+// boolean // true :1 false 0 
+
+let isLoggedIn =  1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+//1-> true ; 0 => false
+// ""=>false  (Empty string) false deta
+//"htesh" => true (filled stirng) true deta
+
+let someNumber = 33 
+
+let stringNumber = String(someNumber)
+
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// 1:14 se 3:14 2 hours today
+
+// ***********Opersations*********************
+
+
+let value = 3 ;
+let negValue = -value
+// console.log(negValue)
+
+
+// console.log(2+2);
+// consle.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2%3);
+
+
+let str1 =  "hello"
+let str2 =  " Priya"
+
+let str3 = str1 +str2
+// console.log(str3); --> string concatenation
+
+
+
+//confusion
+// console.log("1" + 2);
+// console.log(1+"2");
+// console.log("1" + 2 + 2);  //jabki yhan toh pehle 2+2 krke fir 14 return krna chaiye tha  prr nai 
+
+// console.log(1+2+"2");
+//yhan 32 aa rha toh confusion why not 
+
+// console.log(3+4*5%3) //this is not ever  the single way if writing the code
+
+// and iske process jaanne ke liye hsitesh houdhary  english video dekh lein 
+
+
+
+// next tricky aprt 
+console.log(+true); //will give 1 
+// but console
+// console.log(true+) //will give error 
+
+
+// console.log(+"");
+
+
+
+let gameCounter = 100
+// gameCounter++ 
+++gameCounter
+console.log(gameCounter)
+
+//  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment read more about this 
+
