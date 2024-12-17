84e3af96805cf5d1490df0eca61a1256fa5f6fa8
Your Name <adityaraj642003@gmail.com>
2024-12-17 16:40:42 +0530

    phase 1completed

---
+++ lect09.js
@@ -0,0 +1,25 @@
+//Dates  and time 
+// moment is described form january 1st 1970 
+
+
+// console.log(Temporal.Now.instant()) will itin fututre
+
+
+let myDate = new Date();
+console.log(myDate.toString()); //in string format
+
+console.log(myDate.toJSON())
+
+console.log(typeof myDate)
+
+let myCreatedDate = new Date(2024, 11  , 23)
+console.log(myCreatedDate.toString());
+
+// newDate.toLocaleString('default' , {
+//         weekday: "long"
+// })
+
+
+
+
+//3:11 mins completed phase 1 of OG js comlpleted 
