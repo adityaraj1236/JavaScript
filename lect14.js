4fc854deb2a6ce9760c366c50743205c1719532b
Your Name <adityaraj642003@gmail.com>
2024-12-21 12:31:39 +0530

    api json

---
+++ lect14.js
@@ -0,0 +1,15 @@
+// Json and apis 
+
+// destrucuring 
+
+const myObj = {
+    name: "Aditya",
+    age: 25
+}
+
+const name = myObj.name;
+const age = myObj.age;
+
+const {name : kohli } = myObj; //acghaa kohli mtlb uska hamne naya naam de diya hai var name ke liye
+
+console.log(kohli);
